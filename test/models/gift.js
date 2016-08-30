5d02ecc32154a8a8b576cb2bf08ec4aaaf017ab0
stefanocudini <stefano.cudini@gmail.com>
2016-08-30 17:19:34 +0200

    added gift test

--- test/models/gift.js
+++ test/models/gift.js
@@ -12,45 +12,62 @@ define(function () {
         //gift_process_FOOD_AMOUNT_PROC_000042BUR201001
 
         items: [
-          {
-            "name": "group",
-            "parameters": {
-              "by": [
-                "subject",
-                "group_code",
-                "subgroup_code"//optional
-              ],
-              "aggregations": [
-                {
-                  "columns": [ "value" ],
-                  "rule": "SUM"
+            {
+                id: "item_1", //ref [data-item=':id']
+                type: "chart", //chart || map || table || box,
+                config: {
+                    type: "bubblecircle",
+
+/*         TODO........
+                    x: ["year"], //x axis
+                    y: ["value"],//Y dimension
+                    series: ["flowcategory"], // series
+                    aggregationFn: {"value": "sum"},
+                    useDimensionLabelsIfExist: false,// || default raw else fenixtool
+*/
                 },
-                {
-                  "columns": [ "um" ],
-                  "rule": "first"
-                }
-              ]
+                postProcess: [
+                  {
+                    "name": "group",
+                    "parameters": {
+                      "by": [
+                        "subject",
+                        "group_code",
+                        "subgroup_code"//optional
+                      ],
+                      "aggregations": [
+                        {
+                          "columns": [ "value" ],
+                          "rule": "SUM"
+                        },
+                        {
+                          "columns": [ "um" ],
+                          "rule": "first"
+                        }
+                      ]
+                    }
+                  },
+                  {
+                    "name": "group",
+                    "parameters": {
+                      "by": [
+                        "group_code",
+                        "subgroup_code"//optional
+                      ],
+                      "aggregations": [
+                        {
+                          "columns": [ "value" ],
+                          "rule": "AVG"
+                        },
+                        {
+                          "columns": [ "um" ],
+                          "rule": "first"
+                        }
+                      ]
+                    }
+                  }
+                ]
             }
-          },
-          {
-            "name": "group",
-            "parameters": {
-              "by": [
-                "group_code",
-                "subgroup_code"//optional
-              ],
-              "aggregations": [
-                {
-                  "columns": [ "value" ],
-                  "rule": "AVG"
-                },
-                {
-                  "columns": [ "um" ],
-                  "rule": "first"
-                }
-              ]
-            }
-          }
         ]
     }
 
