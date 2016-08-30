b6b0a670c91273e5b28b6a674683155082ee6f4a
stefanocudini <stefano.cudini@gmail.com>
2016-08-30 18:01:49 +0200

    adde treemap test

--- test/models/gift.js
+++ test/models/gift.js
@@ -33,6 +33,28 @@ define(function () {
                     Year: ["2013"]
                 },
                 filterFor: ["IndicatorCode", "GenderCode", "AgeRangeCode", "CountryCode"] // allowed dimension ids to filter,
+            },
+              {
+                id: "item_2", //ref [data-item=':id']
+                type: "chart", //chart || map || olap,
+                config: {
+                    type: "treemap",
+                    useDimensionLabelsIfExist : true,
+                    x: ["Year"], //x axis and series
+                    series: ["CountryCode"], //Y dimension
+                    y: ["Value"],
+                    aggregationFn: {"Value": "sum"}
+                }, // :type-creator config
+               filter: { //FX-filter format
+
+                    IndicatorCode: ["010101"],
+                    GenderCode: ["3"],
+                    AgeRangeCode: ["AGT"],
+                    CountryCode: ["BEN", "BFA", "CAF", "COM", "DJI", "ERI", "GHA", "GNB", "KEN", "LBR",
+                        "LBY", "MRT", "NER", "NGA", "STP", "SEN", "SLE", "SOM", "SDN", "TGO", "TUN"],
+                    Year: ["2013"]
+                },
+                filterFor: ["IndicatorCode", "GenderCode", "AgeRangeCode", "CountryCode"] // allowed dimension ids to filter,
             }
             /*{
                 id: "item_1", //ref [data-item=':id']
