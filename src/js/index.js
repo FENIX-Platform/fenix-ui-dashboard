509ffd45b3e16c010f0aa34e3d8ac3602553915c
tanzm <Tanzila.Mohammad@fao.org>
2016-11-23 13:59:28 +0000

    Fix for Item Ready

--- src/js/index.js
+++ src/js/index.js
@@ -491,7 +491,7 @@ define([
 
         var is = new Item(conf);
 
-        is.on("ready", _.bind(this._onItemReady(item), this), item);
+        is.on("ready", _.bind(this._onItemReady, this, item));
 
         this.itemInstances[item.id] = is
     };
